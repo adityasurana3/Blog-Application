a357a1bff069cc5907bbe147f0ecdc5097a55502
Aditya Surana <aditya.surana@proshore.eu>
2023-11-02 14:22:34 +0545

    custom templates tag added, rss feed added and sitemaps added

--- blog/views.py
+++ blog/views.py
@@ -37,7 +37,11 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year = year, publish__month=month, publish__day = day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    return render(request, 'blog/post/detail.html',{'post':post,'comments':comments,'form':form})
+    # List similar posts 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    return render(request, 'blog/post/detail.html',{'post':post,'comments':comments,'form':form,'similar_posts':similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status = Post.Status.PUBLISHED)
